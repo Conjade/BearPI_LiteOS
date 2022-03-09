e80c0ff9636424d1a62ea4b851f1eab328355dbf
holyCloud <yang@hstc.edu.cn>
2022-03-10 00:58:16 +0800

    MX 生成的用户代码

---
+++ Core/Src/main.c
@@ -0,0 +1,368 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+ /* LiteOS 头文件 */
+#include "los_sys.h"
+#include "los_task.ph"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* 定义任务句柄 */
+UINT32 LED_Task_Handle;
+UINT32 Key_Task_Handle;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+/* 函数声明 */
+static UINT32 AppTaskCreate(void);
+static UINT32 Creat_LED_Task(void);
+static UINT32 Creat_Key_Task(void);
+
+static void LED_Task(void);
+static void Key_Task(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+/*********************************************************************
+  * @ 函数名  ： AppTaskCreate
+  * @ 功能说明： 任务创建，为了方便管理，所有的任务创建函数都可以放在这个函数里面
+  * @ 参数    ： 无  
+  * @ 返回值  ： 无
+  *******************************************************************/
+static UINT32 AppTaskCreate(void)
+{
+	/* 定义一个返回类型变量，初始化为LOS_OK */
+	UINT32 uwRet = LOS_OK;
+	
+	uwRet = Creat_LED_Task();
+  if (uwRet != LOS_OK)
+  {
+		printf("LED_Task任务创建失败！失败代码0x%X\n",uwRet);
+		return uwRet;
+  }
+	
+	uwRet = Creat_Key_Task();
+  if (uwRet != LOS_OK)
+  {
+		printf("Key_Task任务创建失败！失败代码0x%X\n",uwRet);
+		return uwRet;
+  }
+	return LOS_OK;
+}
+
+
+/******************************************************************
+  * @ 函数名  ： Creat_LED_Task
+  * @ 功能说明： 创建LED_Task任务
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ******************************************************************/
+static UINT32 Creat_LED_Task()
+{
+	//定义一个创建任务的返回类型，初始化为创建成功的返回值
+	UINT32 uwRet = LOS_OK;			
+	
+	//定义一个用于创建任务的参数结构体
+	TSK_INIT_PARAM_S task_init_param;	
+
+	task_init_param.usTaskPrio = 5;	/* 任务优先级，数值越小，优先级越高 */
+	task_init_param.pcName = "LED_Task";/* 任务名 */
+	task_init_param.pfnTaskEntry = (TSK_ENTRY_FUNC)LED_Task;/* 任务函数入口 */
+	task_init_param.uwStackSize = 1024;		/* 堆栈大小 */
+
+	uwRet = LOS_TaskCreate(&LED_Task_Handle, &task_init_param);/* 创建任务 */
+	return uwRet;
+}
+/*******************************************************************
+  * @ 函数名  ： Creat_Key_Task
+  * @ 功能说明： 创建Key_Task任务
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ******************************************************************/
+static UINT32 Creat_Key_Task()
+{
+	// 定义一个创建任务的返回类型，初始化为创建成功的返回值
+	UINT32 uwRet = LOS_OK;				
+	TSK_INIT_PARAM_S task_init_param;
+
+	task_init_param.usTaskPrio = 4;	/* 任务优先级，数值越小，优先级越高 */
+	task_init_param.pcName = "Key_Task";	/* 任务名*/
+	task_init_param.pfnTaskEntry = (TSK_ENTRY_FUNC)Key_Task;/* 任务函数入口 */
+	task_init_param.uwStackSize = 1024;	/* 堆栈大小 */
+	
+	uwRet = LOS_TaskCreate(&Key_Task_Handle, &task_init_param);/* 创建任务 */
+
+	return uwRet;
+}
+
+/******************************************************************
+  * @ 函数名  ： LED_Task
+  * @ 功能说明： LED_Task任务实现
+  * @ 参数    ： NULL 
+  * @ 返回值  ： NULL
+  *****************************************************************/
+static void LED_Task(void)
+{
+  /* 任务都是一个无限循环，不能返回 */
+	while(1)
+	{
+		HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13);;      //LED1翻转
+		printf("LED任务正在运行！\n");
+		LOS_TaskDelay(1000);	
+	}
+}
+/******************************************************************
+  * @ 函数名  ： Key_Task
+  * @ 功能说明： Key_Task任务实现
+  * @ 参数    ： NULL 
+  * @ 返回值  ： NULL
+  *****************************************************************/
+static void Key_Task(void)
+{
+	UINT32 uwRet = LOS_OK;				
+	
+	/* 任务都是一个无限循环，不能返回 */
+	while(1)
+	{
+		/* K1 被按下 */
+		if( Key_Scan(KEY1_GPIO_PORT,KEY1_GPIO_PIN) == KEY_ON )
+		{
+			printf("挂起LED任务！\n");
+			uwRet = LOS_TaskSuspend(LED_Task_Handle);/* 挂起LED1任务 */
+			if(LOS_OK == uwRet)
+			{
+				printf("挂起LED任务成功！\n");
+			}
+		}
+		/* K2 被按下 */
+		else if( Key_Scan(KEY2_GPIO_PORT,KEY2_GPIO_PIN) == KEY_ON ) 
+		{
+			printf("恢复LED任务！\n");
+			uwRet = LOS_TaskResume(LED_Task_Handle); /* 恢复LED1任务 */
+			if(LOS_OK == uwRet)
+			{
+				printf("恢复LED任务成功！\n");
+			}
+			
+		}
+		LOS_TaskDelay(20);   /* 20ms扫描一次 */
+	}
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	UINT32 uwRet = LOS_OK;  //定义一个任务创建的返回值，默认为创建成功
+/* LiteOS 内核初始化 */
+	uwRet = LOS_KernelInit();
+	
+  if (uwRet != LOS_OK)
+  {
+		printf("LiteOS 核心初始化失败！失败代码0x%X\n",uwRet);
+		return LOS_NOK;
+  }
+	
+	uwRet = AppTaskCreate();
+	if (uwRet != LOS_OK)
+  {
+		printf("AppTaskCreate创建任务失败！失败代码0x%X\n",uwRet);
+		return LOS_NOK;
+  }
+
+  /* 开启LiteOS任务调度 */
+  LOS_Start();
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 20;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+ /**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
